0a30b60acbf6577911f8c925feb7dbbb9655940b
nishikawa-naonori <watar.wind.xyz1990@gmail.com>
2021-04-11 22:13:47 +0900

    4月11日のjava練習4

---
+++ A015.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;  // パッケージをインポート
+ 
+class A015 {
+  public static void main(String[] args) {
+    Scanner myObj = new Scanner(System.in);  // スキャナーオブジェクトの生成
+    System.out.println("ユーザーネームを入力してください。");
+ 
+    String userName = myObj.nextLine();  // userNameを読み込む。
+    System.out.println("ユーザー名:" + userName);  // userNameを出力。
+
+
+  }
+}
